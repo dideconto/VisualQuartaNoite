e7a2e6ea502c708856b7fa41ac2a8bf819818613
Diogo Deconto <dideconto@gmail.com>
2021-09-22 20:19:06 -0300

    Refatoração da listagem de produtos e navegação entre componentes

--- FRONT/src/app/app.module.ts
+++ FRONT/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
+import { ListarProdutoComponent } from './components/views/produto/listar-produto/listar-produto.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, ListarProdutoComponent],
     imports: [BrowserModule, AppRoutingModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent],
