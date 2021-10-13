a46efe3979dd4f03595cd3f13b7ad7cedc951728
Diogo Deconto <dideconto@gmail.com>
2021-10-13 20:29:16 -0300

    Instalação do Angular Material e criação do cabeçalho

--- FRONT/src/app/app.module.ts
+++ FRONT/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
+//Componentes da aplicação
 import { AppComponent } from "./app.component";
+import { TemplateComponent } from "./components/template/template.component";
 import { CadastrarProdutoComponent } from "./components/views/produto/cadastrar-produto/cadastrar-produto.component";
 import { ListarProdutoComponent } from "./components/views/produto/listar-produto/listar-produto.component";
 
@@ -12,8 +18,18 @@ import { ListarProdutoComponent } from "./components/views/produto/listar-produt
         AppComponent,
         ListarProdutoComponent,
         CadastrarProdutoComponent,
+        TemplateComponent,
     ],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
